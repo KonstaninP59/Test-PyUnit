ae2036a6a77bbfa18ebe4bc7f9ad3a4569e80ff5
KonstaninP59 <konstanin_p@mail.ru>
2025-10-01 19:22:35 +0500

    creature

---
+++ tests/test_calc.py
@@ -0,0 +1,124 @@
+import math
+import unittest
+from parameterized import parameterized
+from app.main import Calculator
+from math import inf
+from app.error import InvalidInputException
+
+
+class TestCalculator(unittest.TestCase):
+    def setUp(self) -> None:
+        self.calc = Calculator()
+
+    def tearDown(self) -> None:
+        ...
+
+    @parameterized.expand(
+        # 1. arrange
+        [
+            ("integers", 2, 3, 5),
+            ("floats", 2.5, 3.1, 5.6),
+            ("negative", -2.5, 3.0, 0.5)
+        ]
+    )
+    def test_sum(self, name, a, b, expected_result):
+        # 2. act
+        actual_result = self.calc.sum(a, b)
+
+        # 3. assert
+        self.assertEqual(actual_result, expected_result)
+
+    @parameterized.expand([
+        ("strings", 'aaa', 'bbb', TypeError),
+        ("int_None", 1, None, TypeError),
+        ("None_float", None, 1.1, TypeError),
+        ("None_None", None, None, TypeError)
+
+    ])
+    def test_sum_invalid_values(self, name, a, b, expected_result):
+        with self.assertRaises(expected_result):
+            self.calc.sum(a, b)
+
+    @parameterized.expand([
+        ("list_integers", [1, 2, 3, 4], 10),
+        ("list_empty", [], 0),
+        ("list_single", [1], 1)
+    ])
+    def test_sum_list(self, name, a, expected_result):
+        # 2. act
+        actual_result = self.calc.sum(*a)
+        # 3. assert
+        self.assertEqual(actual_result, expected_result)
+
+    @parameterized.expand([
+        ("tuple_integers", (1, 2, 3, 4), 10),
+        ("tuple_empty", (), 0),
+        ("tuple_single", (1,), 1),
+        ("set_integers", {1, 2, 3, 4}, 10),
+        ("set_empty", {}, 0),
+        ("set_single", {1}, 1),
+    ])
+    def test_sum_tuple(self, name, a, expected_result):
+        # 2. act
+        actual_result = self.calc.sum(*a)
+        # 3. assert
+        self.assertEqual(actual_result, expected_result)
+
+    def test_multiply(self):
+        a = 5
+        b = 0.000000005
+
+        actual_result = self.calc.multiply(a, b)
+        expected_result = 0
+
+        self.assertAlmostEqual(actual_result, expected_result)
+
+    def test_divide(self):
+        a = 5
+        b = 0
+
+        expected_result = ZeroDivisionError
+
+        with self.assertRaises(expected_result):
+            self.calc.divide(a, b)
+
+    def test_divide_inf(self):
+        a = inf
+        b = inf
+
+        expected_result = None
+        actual_result = self.calc.divide(a, b)
+
+        self.assertNotEqual(actual_result, expected_result)
+        self.assertIsInstance(actual_result, type(math.inf))
+        self.assertIsInstance(actual_result, float)
+
+    def test_log(self):
+        a = 8
+        b = 2
+
+        expected_result = 3
+        actual_result = self.calc.log(a, b)
+
+        self.assertEqual(actual_result, expected_result)
+
+    @parameterized.expand([
+        ('str_int', 'a', 0, TypeError),
+        ('int_str', 0, 'a', TypeError),
+        ('str_none', 'a', None, TypeError),
+    ])
+    def test_log_type(self, name, a, b, expected_result):
+        with self.assertRaises(expected_result):
+            self.calc.log(a, b)
+
+    @parameterized.expand([
+        (-1, 2, InvalidInputException),
+        (1, 2, InvalidInputException),
+    ])
+    def test_log_invalid_input_exception(self, a, b, expected_result):
+        with self.assertRaises(expected_result):
+            self.calc.log(a, b)
+
+
+if __name__ == "__main__":
+    unittest.main()
